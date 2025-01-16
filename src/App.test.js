25197e899d5473d921be0348581d4ac1cfe94727
serhidalgra <serhidalgra@gmail.com>
2025-01-15 23:48:15 -0600

    Uploading my project app to deploy in github pages

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+import React from 'react';
+    import ReactDOM from 'react-dom';
+    import { Provider } from 'react-redux';
+    import storeConfig from './store/storeConfig';
+    import App from './App';
+
+    const store = storeConfig();
+
+    test('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+    });
